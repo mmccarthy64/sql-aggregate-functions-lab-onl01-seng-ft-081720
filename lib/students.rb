495272f133aef823923bf0d4ec58775f0df1d3f9
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-09-20 14:55:50 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
@@ -19,3 +20,22 @@ end
 def average_gpa_for_9th_grade
     "SELECT AVG(gpa) FROM students WHERE grade = 9;"
 end
+=======
+## Code your solution below. Note that your SQL queries should be in quotation marks. 
+
+def highest_student_gpa
+  SELECT MAX()
+end
+
+def lowest_student_gpa
+end
+
+def average_student_gpa
+end
+
+def total_tardies_for_all_students
+end
+
+def average_gpa_for_9th_grade
+end
+>>>>>>> a8550bf3368587327d6060adc9cae4f9293bd75f
